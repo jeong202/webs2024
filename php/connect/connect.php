9f123ade48f7c40a3097ad6d44ba37e80bfe0b75
jeong202 <jhyjhy968@gmail.com>
2023-09-25 17:15:20 +0900

    2023.09/25 PM 5시 15분
    2023.09/25 PM 5시 15분
    수업

---
+++ php/connect/connect.php
@@ -0,0 +1,15 @@
+<?php
+    $host = "localhost";
+    $user = "jhyjhy968";
+    $pw = "dothome369582!";
+    $db = "jhyjhy968";
+
+    $connect = new mysqli($host, $user, $pw, $db);
+    $connect -> set_charset("utf-8");
+
+    if(mysqli_connect_errno()){
+        echo "DATABASE Connect False";
+    } else {
+        echo "DATABASE Connect True";
+    }
+?>
