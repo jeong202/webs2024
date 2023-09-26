fcae49bb29d28ceaca591ee4658e3af4c15e86fe
jeong202 <144635664+jeong202@users.noreply.github.com>
2023-09-27 01:36:28 +0900

    2023.09/27 AM 1시 35분
    2023.09/27 AM 1시 35분

---
+++ php/include/footer.php
@@ -0,0 +1,6 @@
+<footer id="footer" role="contentinfo">
+        <div class="footer__inner container btStyle">
+            <div>Copyright 2023 Jeonghaeyeon</div>
+            <div>blog by Jeonghaeyeon</div>
+        </div>
+</footer>
---
+++ php/include/header.php
@@ -0,0 +1,25 @@
+<header id="header" role="banner">
+    <div class="header__inner container">
+        <div class="left">
+            <a href="../main/main.php">
+                <span class="blind">메인으로</span>
+            </a>
+        </div>
+        <div class="logo">
+            <a href="../main/main.php">Jeong Blog</a>
+        </div>
+        <div class="right">
+            <ul>
+                <li><a href="../join/join.php">회원가입</a></li>
+            </ul>
+        </div>
+    </div>
+    <nav class="nav__inner">
+        <ul>
+            <li><a href="../join/join.php">회원가입</a></li>
+            <li><a href="../login/login.php">로그인</a></li>
+            <li><a href="../board/borar.php">게시판</a></li>
+            <li><a href="../blog/blog.php">블로그</a></li>
+        </ul>
+    </nav>
+</header>
